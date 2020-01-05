bfc9f5ecd30d3a077509c63f0011cb1a3ca91ce7
Mangesh Narkhede <braveleo007@gmail.com>
2020-01-05 21:02:12 +0530

    SearchApi: search api integration

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,18 +1,39 @@
 import React, {useState} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import SearchBar from "../components/SearchBar";
-import { Feather } from '@expo/vector-icons';
+import yelp from '../api/yelp';
+
+
 
 const SearchScreen = () => {
+    const [results, setResults] = useState([]);
     const [term, setTerm] = useState('');
+    const [errorMessage, setErrorMessage] = useState('');
+
+    const searchApi = async () => {
+        try{
+        const response = await yelp.get('/search',{
+            params:{
+                limit: 30,
+                term,
+                location: 'san jose'
+            }
+        });
+        setResults(response.data.businesses);
+    }catch(err){
+        setErrorMessage('Something went wrong!');
+    }
+}
+
     return (
       <View>
         <SearchBar 
             term={term} 
-            onTermChange={newTerm => setTerm(newTerm)} 
-            onTermSubmit={() => console.log('ontermsubmit')}
+            onTermChange={setTerm} 
+            onTermSubmit={searchApi}
             />
-        <Text>{term}</Text>
+        <Text>{errorMessage}</Text>
+        <Text>We have found {results.length} results.</Text>
       </View>
     );
 };
